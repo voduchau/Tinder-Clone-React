cd3bc425ee4884e419aeae88459694f25acde44f
Võ Hậu <voduchau01011997@gmail.com>
2020-08-11 23:46:53 +0700

    chat screen

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import { Link } from 'react-router-dom';
 import './Chat.css';
 
 function Chat({ name, message, timestamp, profilePic }) {
 
     return (
-        <div className="chat">
-            <Avatar alt={name} src={profilePic} className="chat_image" />
-            <div className="chat_details">
-                <h2>{name}</h2>
-                <p>{message}</p>
+        <Link to={`/chat/${name}`}>
+            <div className="chat">
+                <Avatar alt={name} src={profilePic} className="chat_image" />
+                <div className="chat_details">
+                    <h2>{name}</h2>
+                    <p>{message}</p>
+                </div>
+                <p className="chat_timestamp">{timestamp}</p>
             </div>
-            <p className="chat_timestamp">{timestamp}</p>
-        </div>
+        </Link>
     )
 }
 
--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -3,7 +3,8 @@ import Avatar from '@material-ui/core/Avatar';
 import './ChatScreen.css'
 
 
-function ChatScreen() {
+function ChatScreen(props) {
+    console.log(props,'xx')
     const [messages,setMessages] = useState([
         {
             name: "sarah",
@@ -16,17 +17,21 @@ function ChatScreen() {
             message: "Hows it going!"
         },
         {
-            name: "sarah",
-            message: "what's up ?"
+            message: "what's up bro?"
         }
     ])
     return (
         <div className="chatScreen">
             <p className="chatScreen_timestamp">YOUR MATCHED WITH SARAH ON 8/8/2020</p>
             { messages.map(message =>(
+                message.name ? 
                 <div className="chatScreen_message">
                     <Avatar alt={message.name} src={message.image} className="chat_image" />
                     <p className="chatScreen_text">{message.message}</p>
+                </div>
+                :
+                <div className="chatScreen_message">
+                    <p className="chatScreen_textUser">{message.message}</p>
                 </div>
             ))}
         </div>
